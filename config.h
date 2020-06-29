0dcd1216d679008e3fcbf89d1f1c05b9ede19cb8
Jonatan Vela <jonatan.vela@gmx.net>
2020-06-29 22:52:21 +0200

    config update

--- config.h
+++ config.h
@@ -5,19 +5,20 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	/*"monospace:size=10"*/
+	"Fira:pixelsize=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                        fg         bg       */
+	[SchemeNorm]          = { "#ffffff", "#000000" },
+	[SchemeSel]           = { "#000000", "#ffffff" },
+	[SchemeSelHighlight]  = { "#e539e2", "#ffffff" },
+	[SchemeNormHighlight] = { "#e539e2", "#000000" },
+	[SchemeOut]           = { "#00ffff", "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 16;
 
 /*
  * Characters not considered part of a word while deleting words
